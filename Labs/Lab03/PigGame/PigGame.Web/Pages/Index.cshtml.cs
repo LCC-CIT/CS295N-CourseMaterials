98f500e9909efdcd2076553a030984450f16d927
Brian Bird <profbird@gmail.com>
2017-10-11 20:38:51 -0700

    Added a Roll button
    Used a named handler, OnPostRoll

--- Labs/Lab03/PigGame/PigGame.Web/Pages/Index.cshtml.cs
+++ Labs/Lab03/PigGame/PigGame.Web/Pages/Index.cshtml.cs
@@ -9,16 +9,31 @@ namespace PigGame.Web.Pages
 {
     public class IndexModel : PageModel
     {
+        PigLogic game;
+
         [BindProperty]
         public string PlayerName { get; set; }
+        public int Roll { get; set; }
+        public PigLogic Game { 
+            get { return game; }
+        }      
+        
         public void OnGet()
         {
+            game = new PigLogic();
         }
 
         public IActionResult OnPost()
         {
-            var game = new PigLogic();
+            game = new PigLogic();
             game.Player1Name = PlayerName;
+            return Page();
+        }
+
+        public IActionResult OnPostRoll()
+        {
+            game = new PigLogic();
+            Roll = game.RollDie();
             return Page();
         }
     }
