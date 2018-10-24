656d33376716a9ccf3cf0668a8d2f5126698a2a2
Brian Bird <birdb@lanecc.edu>
2018-10-24 11:03:11 -0700

    Code for lab 5

---
+++ Labs/Lab05/DebuggingPractice-GroupA/DebuggingPractice/Controllers/DemoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HttpPractice.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PageTwo()
+        {
+            return View();
+        }
+
+        public IActionResult Quiz()
+        {
+            Random rand = new Random();
+            ViewBag.Number1 = rand.Next(100);
+            ViewBag.Number2 = rand.Next(100);
+            return View();
+        }
+
+        public IActionResult QuizAnswer(int number1, int number2, int answer)
+        {
+            string check = "wrong :-(";
+            if (number1 + number2 == answer)
+                check = "right!";
+            return Content(check);
+        }
+
+        public IActionResult Quiz2()
+        {
+            Random rand = new Random();
+            List<int> numbers = new List<int>();
+            numbers.Add(rand.Next(10));
+            numbers.Add(rand.Next(10));
+            return View(numbers);
+        }
+
+        [HttpPut]
+        public IActionResult Quiz2(int number1, int number2, int answer)
+        {
+            string check = "wrong :-(";
+            if (number1 * number2 == answer)
+                check = "right!";
+            return Content(check);
+        }
+
+
+    }
+}
---
+++ Labs/Lab05/DebuggingPractice-GroupB/DebuggingPracticeB/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NickName(string name, string nick)
+        {
+            return Content("Name: " + name + ", Nickname: " + nick);
+        }
+
+        [HttpPost]
+        public IActionResult FavFood(string main, string vegetable, string desert)
+        {
+            return Content("Main dish: " + main + ", Veggie: " + vegetable + ", Desert: " + desert);
+        }
+
+        [HttpPost]
+        public IActionResult FavoriteMusic(string genre, string artist, string song)
+        {
+            return Content("Genre: " + genre + ", Artist: " + artist + ", Song: " + song);
+        }
+
+    }
+
+}
